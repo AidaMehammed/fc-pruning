996c19659550e1298e7e9a10c761e9413c3d2f6d
AidaMehammed <150441666+AidaMehammed@users.noreply.github.com>
2024-03-22 14:05:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(name="fc_pruning",
+setuptools.setup(name="fc-pruning",
                  version="0.0.1",
                  author="Aida Mehammed",
                  author_email="aida.mehammed@studium.uni-hamburg.de",
                  description="FC-Pruning",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 url="https://github.com/FeatureCloud/fc_pruning",
+                 url="https://github.com/FeatureCloud/fc-pruning",
                  project_urls={
                      "Bug Tracker": "https://github.com/AidaMehammed/fc-pruning",
                  },
