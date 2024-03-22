54db24a4cfbd9593c9477e46ffaf5be5b0d70305
AidaMehammed <150441666+AidaMehammed@users.noreply.github.com>
2024-03-22 14:04:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,25 +5,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(name="fc_pruning",
                  version="0.0.1",
-                 author="FeatureCloud",
+                 author="Aida Mehammed",
                  author_email="aida.mehammed@studium.uni-hamburg.de",
-                 description="Image Classification Pruning",
+                 description="FC-Pruning",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
                  url="https://github.com/FeatureCloud/fc_pruning",
                  project_urls={
-                     "Bug Tracker": "https://github.com/FeatureCloud/app-template/issues",
+                     "Bug Tracker": "https://github.com/AidaMehammed/fc-pruning",
                  },
+                 packages=setuptools.find_packages(include=['Compress.*']),
+
                  classifiers=[
                      "Programming Language :: Python :: 3",
                      "Operating System :: OS Independent",
                  ],
-                 packages=setuptools.find_packages(include=['FeatureCloud', 'FeatureCloud.*']),
                  python_requires=">=3.7",
-                 entry_points={'console_scripts': ['FeatureCloud = FeatureCloud.api.cli.__main__:fc_cli',
-                                                   'featurecloud = FeatureCloud.api.cli.__main__:fc_cli',
-                                                   ]
-                               },
-                 install_requires=['featurecloud','torch_pruning','bios','torchvision']
+                 
+                 install_requires=['featurecloud','torch_pruning']
 
                  )
