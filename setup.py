daa324dbbee3dc6441f792787593b1beb2266bf7
AidaMehammed <150441666+AidaMehammed@users.noreply.github.com>
2024-04-30 14:35:10 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(name="fc-pruning",
-                 version="0.0.1",
+setuptools.setup(name="fc_pruning",
+                 version="0.0.4",
                  author="Aida Mehammed",
                  author_email="aida.mehammed@studium.uni-hamburg.de",
-                 description="FC-Pruning",
+                 description="FC Pruning",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 url="https://github.com/AidaMehammed/fc-pruning",
+                 url="https://github.com/AidaMehammed/fc_pruning",
                  project_urls={
-                     "Bug Tracker": "https://github.com/AidaMehammed/fc-pruning",
+                     "Bug Tracker": "https://github.com/AidaMehammed/fc_pruning",
                  },
                  packages=setuptools.find_packages(include=['Compress','Compress.*']),
 
@@ -20,7 +20,7 @@ setuptools.setup(name="fc-pruning",
                      "Programming Language :: Python :: 3",
                      "Operating System :: OS Independent",
                  ],
-                 python_requires=">=3.7",
+                 python_requires=">=3.8",
                  
                  install_requires=['featurecloud','torch_pruning']
 
